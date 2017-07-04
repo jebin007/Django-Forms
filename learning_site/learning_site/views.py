debc5c436521ea31f515b4082d60d211322aec1e
jebin007 <jevinsubedi@gmail.com>
2017-07-04 18:12:02 -0500

    Handling a Form in a View: -added a function in view when user submits the form ie. (if request.method =='POST') -sends a form as an email if the form is valid -flash a message on successful form submission -redirect back to the suggestion url with flashed message on successful form submission.

--- learning_site/learning_site/views.py
+++ learning_site/learning_site/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
 from . import forms
 
 
@@ -7,4 +13,15 @@ def hello_world(request):
 
 def suggestion_view(request):
     form = forms.SuggestionForm()  #instantiated a copy of our form
+    if request.method == 'POST':
+        form = forms.SuggestionForm(request.POST) #fills in the data from the request
+        if form.is_valid():
+            send_mail(
+                'Suggestion from {}'.format(form.cleaned_data['name']),#from
+                form.cleaned_data['suggestion'],#suggestion
+                '{name} <{email}>'.format(**form.cleaned_data),#emailfrom
+                ['jevinsubedi@gmail.com']
+            )
+            messages.add_message(request, messages.SUCCESS, 'Thanks for your suggestion!')
+            return HttpResponseRedirect(reverse('suggestion'))
     return render(request, 'suggestion_form.html', {'form': form})
