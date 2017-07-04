d362e51f8f7c278676d2fef3dca9a542c6568578
jebin007 <jevinsubedi@gmail.com>
2017-07-04 16:28:42 -0500

    Created a url, view and suggestion_form.html to make it work when browsed.

--- learning_site/learning_site/views.py
+++ learning_site/learning_site/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from . import forms
 
 
 def hello_world(request):
     return render(request, 'home.html')
+
+def suggestion_view(request):
+    form = forms.SuggestionForm()  #instantiated a copy of our form
+    return render(request, 'suggestion_form.html', {'form': form})
