5b34c0288af67a4b749bee037c5a31aacbab9e87
jebin007 <jevinsubedi@gmail.com>
2017-07-04 19:38:31 -0500

    Using and Creating Validators: -another easy of using form validator to prevent bots!

--- learning_site/learning_site/forms.py
+++ learning_site/learning_site/forms.py
@@ -1,14 +1,18 @@
 from django import forms
+from django.core import validators
+
+def must_be_empty(value):
+    if value:
+        raise forms.ValidationError('is not empty')
 
 
 class SuggestionForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     suggestion = forms.CharField(widget=forms.Textarea)
-    honeypot = forms.CharField(required=False, widget=forms.HiddenInput, label="Leave empty")
+    honeypot = forms.CharField(required=False,
+                               widget=forms.HiddenInput,
+                               label="Leave empty",
+                               validators=[must_be_empty])
 
-    def clean_honeypot(self):
-        honeypot = self.cleaned_data['honeypot']
-        if len(honeypot):
-            raise forms.ValidationError("honeypot should be left empty. Bad bot!")
-        return honeypot
+
