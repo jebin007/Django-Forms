92f5065e539e7bccdc7282e5cec30f8491736928
jebin007 <jevinsubedi@gmail.com>
2017-07-04 20:32:58 -0500

    Cleaning a Whole Form -added a clean(self) method to verify the email address entered twice are the same!

--- learning_site/learning_site/forms.py
+++ learning_site/learning_site/forms.py
@@ -9,10 +9,17 @@ def must_be_empty(value):
 class SuggestionForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label="Please verify your email address")
     suggestion = forms.CharField(widget=forms.Textarea)
     honeypot = forms.CharField(required=False,
                                widget=forms.HiddenInput,
                                label="Leave empty",
                                validators=[must_be_empty])
 
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        verify = cleaned_data.get('verify_email')
 
+        if email != verify:
+            raise forms.ValidationError("You need to enter same email in both fields!")
